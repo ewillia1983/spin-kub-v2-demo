328ff3b590989790ebd733df24b4ef5efd6c5b57
Eric Williams <eric.williams@lexisnexis.com>
2019-05-31 09:27:45 -0400

    update main go

--- main.go
+++ main.go
@@ -1,28 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
-	"os"
-	"fmt"
 	"net/http"
+	"os"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Handling %+v\n", r);
-	bs, err := ioutil.ReadFile("/content/index.html")
+
+	fmt.Printf("Handling %+v\n", r)
+
+	feature := os.Getenv("NEW_FEATURE")
+
+	var content string
+	if feature == "true" {
+		content = "/content/index.html"
+	} else {
+		content = "/content/newFeature.html"
+	}
+
+	data, err := ioutil.ReadFile(content)
 
 	if err != nil {
-		fmt.Printf("Couldn't read index.html: %v", err);
+		fmt.Printf("Couldn't read index.html: %v", err)
 		os.Exit(1)
 	}
 
-	io.WriteString(w, string(bs[:]))
+	io.WriteString(w, string(data[:]))
 }
 
 func main() {
 	http.HandleFunc("/", index)
 	port := ":8000"
-	fmt.Printf("Starting to service on port %s\n", port);
+	fmt.Printf("Starting to service on port %s\n", port)
 	http.ListenAndServe(port, nil)
 }
